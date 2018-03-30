bf2e072ddfb710926b72fa7b9936b0f29c126c59
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2018-03-30 00:18:30 -0700

    adding chat and active player features

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var path = require('path');
 var server = require('http').Server(app).listen(process.env.PORT || 3000, ()=>{
   console.log("Listening for connections");
 });
-var io = require('socket.io')(server);
+var io = require('socket.io')(server, { wsEngine: 'ws' });
 var Chess = require('chess.js').Chess;
 var chess = new Chess();
-//var nickname = "";
-//var users = [];
+var users = [];
+//var messages = [];
+var name = "";
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,6 +21,7 @@ app.get('/', (req, res)=>{
 
 io.of('/').on('connection', function(socket) {
   console.log("New socket connected");
+  socket.emit('displayUsers', {total : users.length, all_users : users});
 
   socket.on('newMove', function(data) {
     //if valid move, then update the board
@@ -33,8 +35,23 @@ io.of('/').on('connection', function(socket) {
     io.sockets.emit('switchKing', data);
   });
 
-  socket.on('disconnect', function() {
-    chess.reset();
-    console.log("Socket disconnected");
+  socket.on('newUser', function(data) {
+    // TODO: load all previous messages
+    users.push(data);
+    name = data;
+    io.sockets.emit('appendUser', data);
+   });
+
+  socket.on('newMessage', function(data) {
+    //messages.push(data.message);
+    io.sockets.emit('appendMessage', data);
+   });
+
+   socket.on('disconnect', function() {
+     var index = users.indexOf(name);
+     users.splice(index, 1);
+     io.sockets.emit('updateUsers', users);
+     chess.reset();
+     console.log('disconnected');
   });
 });
--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,54 @@
-//var name = "";
+var name = "";
 var socket = io.connect();
+
+$(function() {
+  $('#formJoin').submit(function(e) {
+    e.preventDefault();
+    if($('#input-name').val() != "")
+    {
+      name = $('#input-name').val();
+      socket.emit('newUser', name);
+      $('#joinGame').hide();
+      $('#game').css({'visibility': 'visible'});
+      $('#active').css({'visibility': 'visible'});
+      $('#chat').css({'visibility': 'visible'});
+    }
+  });
+
+  function addUser(name)
+  {
+    $('#active-users').append('<li class="list-group-item">'+name+'</li>');
+  }
+
+  socket.on('displayUsers', function(data) {
+    var size = data.total;
+    for(var key in data.all_users)
+      addUser(data.all_users[key]);
+  });
+
+  socket.on('appendUser', function(data) {
+    addUser(data);
+  });
+
+  socket.on('updateUsers', function(data) {
+    $('#active-users').empty();
+    for(var key in data)
+      addUser(data[key]);
+  });
+
+  $('#submitMessage').submit(function(e) {
+    e.preventDefault();
+    var userInput = $('#input-message').val();
+    if(userInput)
+       socket.emit('newMessage', {username: name, message: userInput});
+    $('#input-message').val('');
+  });
+
+  socket.on('appendMessage', function(data) {
+    $('#messages').append('<li class="list-group-item"><span class="name">'
+                  + data.username + ': </span>' + data.message + '</li>');
+  });
+});
 
 socket.on('switchKing', function(data) {
   var ctx = document.getElementById('pieces').getContext('2d');
