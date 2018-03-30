0f2a37782262dba8d9badb7aab564cdfe0ddcf2a
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2018-03-29 19:46:05 -0700

    changing port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var server = require('http').Server(app).listen(3000, ()=>{
+var server = require('http').Server(app).listen(process.env.PORT || 3000, ()=>{
   console.log("Listening on port 3000");
 });
 var io = require('socket.io')(server);
